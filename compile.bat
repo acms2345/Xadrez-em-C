cada22569cd1dca82231a62725476ba220ae3526
acms2345 <ancamasi.2345@gmail.com>
2025-12-23 11:42:46 -0300

    Recompilação com configuração ajustada

--- compile.bat
+++ compile.bat
@@ -5,16 +5,13 @@ REM Cria a pasta docs se nao existir
 if not exist docs mkdir docs
 
 REM Compila os arquivos da pasta src para a pasta docs
-"C:\Users\Antonio Carlos\emsdk\upstream\emscripten\emcc.bat" src/menu.c src/xadrez.c src/jogadasvalidas.c -o docs/xadrez.js -s WASM=1 -s EXPORTED_RUNTIME_METHODS="[\"ccall\",\"cwrap\"]" -s EXPORTED_FUNCTIONS="[\"_main\"]" -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=0 -s INVOKE_RUN=1 -s MODULARIZE=0 -O3
+"C:\Users\Antonio Carlos\emsdk\upstream\emscripten\emcc.bat" src/menu.c src/xadrez.c src/jogadasvalidas.c -o docs/xadrez.js -s WASM=1 -s EXPORTED_RUNTIME_METHODS=['ccall','cwrap','FS'] -s EXPORTED_FUNCTIONS=['_main'] -s ALLOW_MEMORY_GROWTH=1 -s EXIT_RUNTIME=0 -s INVOKE_RUN=1 -s MODULARIZE=0 -s ENVIRONMENT=web -O3
 
 if %ERRORLEVEL% EQU 0 (
     echo Compilacao concluida com sucesso!
     echo Arquivos gerados em docs/:
     echo    - xadrez.js
     echo    - xadrez.wasm
-    echo.
-    echo Para testar localmente:
-    echo    cd docs ^&^& python -m http.server 8000
 ) else (
     echo Erro na compilacao
     exit /b 1
